0bcee11e3af7178de04c918f2a62bf21b257e83e
raquerh <raquerh@gmail.com>
2024-05-09 17:38:29 -0300

    agregando JS

---
+++ static/js/datos.js
@@ -0,0 +1,65 @@
+
+
+let tendencia = [
+    {
+        "id": 1,
+        "name": "The Beekeeper",
+        "image": "./static/img/peli_1.jpg",
+    },
+    {
+        "id": 2,
+        "name": "Badland Hunters",
+        "image": "./static/img/peli_2.jpg",
+    },
+    {
+        "id": 3,
+        "name": "The Marvels",
+        "image": "./static/img/peli_3.jpg",
+    },
+    {
+        "id": 4,
+        "name": "Wonka",
+        "image": "./static/img/peli_4.jpg",
+    },
+    {
+        "id": 5,
+        "name": "Migration",
+        "image": "./static/img/peli_5.jpg",
+    },
+    {
+        "id": 6,
+        "name": "Aquaman and the Lost Kingdom",
+        "image": "./static/img/peli_6.jpg",
+    },
+    {
+        "id": 7,
+        "name": "60 Minutes",
+        "image": "./static/img/peli_7.jpg",
+    },
+    {
+        "id": 8,
+        "name": "Wish",
+        "image": "./static/img/peli_8.jpg",
+    },
+    {
+        "id": 9,
+        "name": "The Masked Saint",
+        "image": "./static/img/peli_9.jpg",
+    },
+    {
+        "id": 10,
+        "name": "Due Justice",
+        "image": "./static/img/peli_10.jpg",
+    },
+    {
+        "id": 11,
+        "name": "Orion and the Dark",
+        "image": "./static/img/peli_11.jpg",
+    },
+    {
+        "id": 12,
+        "name": "Genghis Khan",
+        "image": "./static/img/peli_12.jpg",
+    }
+
+]
